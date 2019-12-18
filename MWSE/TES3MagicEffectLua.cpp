6c87f63c3c66f77f8503c2d8ad3ef59027c317f3
Sebastien Levy <sebastien_levy@mentor.com>
2019-12-18 11:04:09 +0100

    dynamic magic effect description
    - stopped messing up with lua properties and used optional stuff to
      handle nil values

--- MWSE/TES3MagicEffectLua.cpp
+++ MWSE/TES3MagicEffectLua.cpp
@@ -119,7 +119,7 @@ namespace mwse {
 				usertypeDefinition.set("name", sol::readonly_property([](TES3::MagicEffect& self) { return TES3::DataHandler::get()->nonDynamicData->magicEffects->getEffectName(self.id); }));
 				usertypeDefinition.set( "description", sol::property(
 					[]( TES3::MagicEffect &self ) { return self.description; },
-					[]( TES3::MagicEffect &self, const char *value ) { self.setDescription( value ); } ) );
+					[]( TES3::MagicEffect &self, sol::optional< const char * >value ) { self.setDescription( value.value_or( nullptr ) ); } ) );
 				usertypeDefinition.set("areaSoundEffect", sol::readonly_property([](TES3::MagicEffect& self) { return self.areaSoundEffect; }));
 				usertypeDefinition.set("boltSoundEffect", sol::readonly_property([](TES3::MagicEffect& self) { return self.boltSoundEffect; }));
 				usertypeDefinition.set("castSoundEffect", sol::readonly_property([](TES3::MagicEffect& self) { return self.castSoundEffect; }));
@@ -129,10 +129,6 @@ namespace mwse {
 					[](TES3::MagicEffect& self, const char* value) { if (strlen(value) < 32) strcpy(self.icon, value); }
 				));
 				usertypeDefinition.set("particleTexture", sol::readonly_property([](TES3::MagicEffect& self) { return self.particleTexture; }));
-
-				// custom functions
-				// that one looks needed (though maybe crap) as setting description to nil using lua is not handled in the property...don't know why, NullCascade, any idea?
-				usertypeDefinition.set( "resetDescription", []( TES3::MagicEffect &self ) { self.setDescription( nullptr ); } );
 
 				// Finish up our usertype.
 				state.set_usertype("tes3magicEffect", usertypeDefinition);
