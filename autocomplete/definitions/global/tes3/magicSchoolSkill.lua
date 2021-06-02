1cc408f957f0d2d24823fbb461ba662e37574632
C3pa <41503714+C3pa@users.noreply.github.com>
2021-06-02 13:58:30 +0200

    tabs

--- autocomplete/definitions/global/tes3/magicSchoolSkill.lua
+++ autocomplete/definitions/global/tes3/magicSchoolSkill.lua
@@ -2,7 +2,7 @@ return {
 	type = "value",
 	description = [[These constants are used to convert magic schools to their respective skill. These constants map to their respective tes3.skill constants.]],
 	valuetype = "tes3.skill.* constants",
-    examples = {
+	examples = {
 		["convertMagicSchoolToSkill"] = {
 			title = "Convert magic school from event data to a respective skill",
 			description = "The example below gives the player 100 experience for failing to cast a spell. Here we can see how tes3.magicSchoolSkill is used."
