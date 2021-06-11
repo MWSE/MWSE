20893b5234db11e27135e49770fc0914e0c8f65f
C3pa <41503714+C3pa@users.noreply.github.com>
2021-06-11 11:45:45 +0200

    tes3spell docs

---
+++ autocomplete/definitions/namedTypes/tes3spell/playerStart.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[Friendly access to playerStart flag. It determines whether this spell be one of spells given to player at the start of the game, based on his/hers skills.]],
+	valuetype = "boolean",
+}
