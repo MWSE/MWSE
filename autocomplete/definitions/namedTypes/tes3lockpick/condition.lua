d2493e8d18728e0db42fef32979d164a1747ce4d
C3pa <41503714+C3pa@users.noreply.github.com>
2021-07-23 15:05:28 +0200

    tes3lockpick

--- autocomplete/definitions/namedTypes/tes3lockpick/condition.lua
+++ autocomplete/definitions/namedTypes/tes3lockpick/condition.lua
@@ -1,5 +1,7 @@
 return {
 	type = "value",
-	description = [[The maximum condition/health of the object.]],
+	description = [[This function is depracted. Use tes3lockpick.maxCondition instead.
+
+The maximum condition/health of the object.]],
 	valuetype = "number",
 }
