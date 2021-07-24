f6da94c422ef53d0086d0ad229e63d6eb1bb86e5
C3pa <41503714+C3pa@users.noreply.github.com>
2021-07-24 18:53:25 +0200

    Update playVisualEffect.lua

--- autocomplete/definitions/namedTypes/tes3magicSourceInstance/playVisualEffect.lua
+++ autocomplete/definitions/namedTypes/tes3magicSourceInstance/playVisualEffect.lua
@@ -1,7 +1,7 @@
 return {
 	type = "method",
 	description = "This function plays an effect from tes3magicSourceInstance of a given index.",
-	arguments = {
+	tableParams = {
 		{ name = "effectIndex", type = "number", description = "The index in the effect list which will be played, a number in range [0, 7]." },
 		{ name = "position", type = "tes3vector3|table", description = "A table or a tes3vector3 holding x, y and z coordinates at which the visual effect will be played." },
 		{ name = "visual", type = "tes3physicalObject|string", description = "" },
