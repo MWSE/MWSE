f92bf2a901b2c88cafad63d6937d951543dfa1fc
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-06-17 02:57:13 +0200

    Create printNodeTree.lua

---
+++ autocomplete/definitions/global/debug/printNodeTree.lua
@@ -0,0 +1,7 @@
+return {
+	type = "function",
+	description = [[Prints node name tree structure of passed niNode object.]],
+	arguments = {
+		{ name = "root", type = "niNode", description = "The root node to traverse and print the tree for." },
+	}
+}
