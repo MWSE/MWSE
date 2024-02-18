081f495a6010c3857b7c653bafef6854cc6136da
herbert100 <154224164+herbertgithubaccount@users.noreply.github.com>
2024-02-18 14:17:57 +0100

    json stuff

--- autocomplete/definitions/global/json/loadfile.lua
+++ autocomplete/definitions/global/json/loadfile.lua
@@ -3,7 +3,8 @@ return {
 	description = [[Loads the contents of a file through json.decode. Files loaded from Data Files\\MWSE\\{fileName}.json.
 
 !!! warning
-	If the table encoded as json had both string and integer indices, this process converted all the integer indices to strings. For example, `[1]` was converted to `["1"]`. So, when loading mixed tables from json, this needs to be considered. If you need to save and load your mod's configuration file, consider using [`mwse.loadConfig()`](https://mwse.github.io/MWSE/apis/mwse/#mwseloadconfig) as that function will do this for you.
+	All of the keys in the `table` returned by function will be `string`s. For example, if you're decoding an array-style table, then the first entry will be `["1"]` instead of `[1]`.
+	If you're using this function to load your mod's configuration file, consider using [`mwse.loadConfig()`](https://mwse.github.io/MWSE/apis/mwse/#mwseloadconfig) instead.
 ]],
 	arguments = {
 		{ name = "fileName", type = "string" },
