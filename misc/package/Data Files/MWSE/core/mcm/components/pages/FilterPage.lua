2daeb66e6ac27d39ac7dc3fbb032d0ea4ff39f6f
herbert100 <154224164+herbertgithubaccount@users.noreply.github.com>
2025-08-20 11:25:38 +0200

    Change `self.__index = self` syntax to `Class.__index = Class`
    The `self.__index = self` paradigm has caused several bugs in the past.
    I think this is because `self.__index = self` obfuscates the `table`/`metatable` relationship and this can lead to some unexpected behavior because
    1. Unlike very other method in the codebase, the `self` in `Class:new` refers to the `Class`, rather than the object being created. This has historically led to bugs where `self` gets modified in `Class:new` under the assumption that the object is being changed, instead of the Class.
    2. `self.__index = self` updates the _Class_ every time a new _object_ is created. This has historically led to problems where the `__index` metamethods of certain classes were overridden as soon as a new object was created.
    
    Hopefully, using `Class.__index = Class` prevents these bugs because it makes the metatable relationships clearer.

--- misc/package/Data Files/MWSE/core/mcm/components/pages/FilterPage.lua
+++ misc/package/Data Files/MWSE/core/mcm/components/pages/FilterPage.lua
@@ -6,6 +6,7 @@ local Parent = require("mcm.components.pages.SideBarPage")
 
 --- @class mwseMCMFilterPage
 local FilterPage = Parent:new()
+FilterPage.__index = FilterPage
 FilterPage.placeholderSearchText = mwse.mcm.i18n("Search...")
 
 function FilterPage:filterComponents()
