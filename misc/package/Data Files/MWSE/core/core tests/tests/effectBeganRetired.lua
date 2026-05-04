959af70d79fc93f01396f761d1467d46d260d962
NullCascade <NullCascade@gmail.com>
2026-05-04 10:30:39 -0700

    Add test for new magicEffectBegan/Retired events.

---
+++ misc/package/Data Files/MWSE/core/core tests/tests/effectBeganRetired.lua
@@ -0,0 +1,74 @@
+local log = mwse.Logger.new({ name = "coreTest:effectBeganRetired" })
+local inspect = require("inspect")
+
+--- A table of all active effects for all active references.
+--- @type table<tes3reference, table<tes3.effect, number>>
+local activeEffects = {}
+
+--- Track effects being added.
+--- @param e magicEffectBeganEventData
+local function onMagicEffectBegan(e)
+	log:info("Magic effect %s began on %s", e.effect, e.target)
+	local refEffects = table.getset(activeEffects, e.target, {})
+	refEffects[e.effect.id] = (refEffects[e.effect.id] or 0) + 1
+end
+event.register(tes3.event.magicEffectBegan, onMagicEffectBegan)
+
+--- Track effects being removed.
+--- @param e magicEffectBeganEventData
+local function onMagicEffectRetired(e)
+	log:info("Magic effect %s retired on %s", e.effect, e.target)
+
+	local refEffects = activeEffects[e.target]
+	if (not refEffects) then
+		log:error("Attempting to remove effect '%s' from reference '%s': No effects are registered to this reference.", e.effect, e.target)
+		os.exit()
+	end
+
+	local currentCount = refEffects[e.effect.id]
+	if (not currentCount) then
+		log:error("Attempting to remove effect '%s' from reference '%s': The effect ID isn't registered to the reference.", e.effect, e.target)
+		os.exit()
+	end
+
+	local newCount = currentCount - 1
+	if (newCount < 0) then
+		log:error("Attempting to remove effect '%s' from reference '%s': The effect count became negative.", e.effect, e.target)
+		os.exit()
+	elseif (newCount == 0) then
+		refEffects[e.effect.id] = nil
+		if (table.empty(refEffects)) then
+			activeEffects[e.target] = nil
+		end
+	else
+		refEffects[e.effect.id] = newCount
+	end
+end
+event.register(tes3.event.magicEffectRetired, onMagicEffectRetired)
+
+local function testActiveEffects()
+	-- Gather list of all mobiles. Note that the player is not in the main list.
+	local allMobiles = table.values(tes3.worldController.mobManager.processManager.allMobileActors)
+	assert(table.find(allMobiles, tes3.mobilePlayer) == nil)
+	table.insert(allMobiles, tes3.mobilePlayer)
+
+	local calculatedEffects = {}
+	for _, mobile in ipairs(allMobiles) do
+		if (mobile.reference) then
+			local mobileEffects = {}
+			for _, effect in ipairs(mobile.activeMagicEffectList) do
+				mobileEffects[effect.effectId] = (mobileEffects[effect.effectId] or 0) + 1
+			end
+			if (not table.empty(mobileEffects)) then
+				assert(calculatedEffects[mobile.reference] == nil)
+				calculatedEffects[mobile.reference] = mobileEffects
+			end
+		end
+	end
+
+	if (not table.equal(activeEffects, calculatedEffects)) then
+		log:error("Active effect test failed:\n\nTracked: %s\n\nCalculated: %s", inspect(activeEffects), inspect(calculatedEffects))
+		os.exit()
+	end
+end
+event.register(tes3.event.simulate, testActiveEffects)
