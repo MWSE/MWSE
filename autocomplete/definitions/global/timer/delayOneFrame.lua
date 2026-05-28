bdf3a5ea8f912faae067305432e21160c7309f6c
NullCascade <NullCascade@gmail.com>
2026-05-28 01:25:36 -0700

    Add aliases for MWSE timer types and states.

--- autocomplete/definitions/global/timer/delayOneFrame.lua
+++ autocomplete/definitions/global/timer/delayOneFrame.lua
@@ -6,7 +6,7 @@ return {
 	It's recommended to study the [Object Lifetimes](../guides/object-lifetimes.md) guide. It describes how to safely use [tes3reference](../types/tes3reference.md) objects inside timer callbacks.]],
 	arguments = {
 		{ name = "callback", type = "fun(e: mwseTimerCallbackData)", description = "The callback function that will execute when the timer expires." },
-		{ name = "type", type = "integer", optional = true, default = "`timer.simulate`", description = "Type of the timer. This value can be `timer.simulate`, `timer.game` or `timer.real`." },
+		{ name = "type", type = "integer|mwseTimer.type", optional = true, default = "`timer.simulate`", description = "Type of the timer. This value can be `timer.simulate`, `timer.game` or `timer.real`." },
 	},
 	returns = { name = "timer", type = "mwseTimer" },
 }
