76f9af222acc48a0ca2b77aa59636c594ae67c8e
C3pa <41503714+C3pa@users.noreply.github.com>
2026-08-12 13:35:50 +0200

    Remove throw from StdString
    This class isn't meant to be used in the CSSE. All the other methods do throw, so probably removing throw only from the destructor doesn't cause any problems

--- SharedSE/StdString.cpp
+++ SharedSE/StdString.cpp
@@ -23,12 +23,8 @@ namespace se {
 	}
 
 	StdString::~StdString() {
-#if defined(SE_TARGETS_MW) && SE_TARGETS_MW == 1
 		const auto TES3_StdString_dtor = reinterpret_cast<void(__thiscall**)(StdString*)>(0x7461C4);
 		(*TES3_StdString_dtor)(this);
-#else
-		throw not_implemented_exception();
-#endif
 	}
 
 	void* StdString::operator new(size_t size) {
