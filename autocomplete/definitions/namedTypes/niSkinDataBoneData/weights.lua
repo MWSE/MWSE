e5388ace9ddbd8e01a14d73bd7f237230f46fbc1
longod <forferelden@gmail.com>
2026-08-15 10:43:06 +0900

    Fix the vertices typo in definitions

--- autocomplete/definitions/namedTypes/niSkinDataBoneData/weights.lua
+++ autocomplete/definitions/namedTypes/niSkinDataBoneData/weights.lua
@@ -1,6 +1,6 @@
 return {
 	type = "value",
-	description = [[Contains all the per-vertex weight coefficients used when deforming the verticies influenced by this bone.]],
+	description = [[Contains all the per-vertex weight coefficients used when deforming the vertices influenced by this bone.]],
 	readOnly = true,
 	valuetype = "niSkinDataBoneDataVertexWeight[]",
 }
